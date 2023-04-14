4d539e08be1bebac6ad00903e36fa8772e1f2f52
Gokul001 <gokuljune2003@gmail.com>
2023-04-14 17:47:11 +0530

    added new changes

--- main.py
+++ main.py
@@ -22,68 +22,72 @@ def on_connect(client, userdata, flags, rc, properties=None):
     # subscribe to the topic when connecte
     client.subscribe("print/orderdetails", qos=1)
     client.subscribe("print/testing", qos=1)
+    client.publish("printer/status", 'Printer Ready!')
 
 
 def on_message(client, userdata, msg):
-    try:
-        # decode the incoming message
-        data = json.loads(msg.payload.decode("utf-8"))
+    if msg.topic == "printer/status":
+        print("Status send")
+    else:
+        try:
+            # decode the incoming message
+            data = json.loads(msg.payload.decode("utf-8"))
 
-        # establish printer connection
-        printer = Usb(VENDOR_ID, PRODUCT_ID)
+            # establish printer connection
+            printer = Usb(VENDOR_ID, PRODUCT_ID)
 
-        # set up receipt header
-        printer.set("center", "a", "b", 1, 1)
-        printer.text("SRI SARAVANA\n")
-        printer.text("NH SERVICE ROAD\n")
-        printer.text("MELMARUVATHUR -603319\n")
-        printer.text("CONTACT - 99652587272\n\n")
+            # set up receipt header
+            printer.set("center", "a", "b", 1, 1)
+            printer.text("SRI SARAVANA\n")
+            printer.text("NH SERVICE ROAD\n")
+            printer.text("MELMARUVATHUR -603319\n")
+            printer.text("CONTACT - 99652587272\n\n")
 
-        # print order number, bill number, time and date
-        printer.set("left", "a", "b", 1, 1)
-        # print order number, bill number, time and date
-        printer.set("left", "a", "b", 1, 1)
-        # printer.text("Order No: "+ data["orderno"] +"\n")
-        printer.set("right", "a", "b", 1, 1)
-        printer.text("Time: " + data["timestamp"] + "\n")
-        printer.text("Date: " + data["date"] + "\n")
-        printer.set("left", "a", "b", 1, 1)
-        printer.text("Bill No: " + str(data["bill_number"]) + "\n\n")
-        printer.set('center', 'a', 'b', 1, 1)
+            # print order number, bill number, time and date
+            printer.set("left", "a", "b", 1, 1)
+            # print order number, bill number, time and date
+            printer.set("left", "a", "b", 1, 1)
+            # printer.text("Order No: "+ data["orderno"] +"\n")
+            printer.set("right", "a", "b", 1, 1)
+            printer.text("Time: " + data["timestamp"] + "\n")
+            printer.text("Date: " + data["date"] + "\n")
+            printer.set("left", "a", "b", 1, 1)
+            printer.text("Bill No: " + str(data["bill_number"]) + "\n\n")
+            printer.set('center', 'a', 'b', 1, 1)
 
-        # set up receipt items
-        items = data["items"]
+            # set up receipt items
+            items = data["items"]
 
-        # print item details and calculate subtotal
-        subtotal = 0
-        # printer.text("-----------------------------------------------\n")
-        printer.text("{:<20}{:<8}{:<10}\n".format("Item", "Qty", "Price"))
-        printer.text("-----------------------------------------------\n")
-        for item in items:
-            printer.text("{:<20}{:<8}{:<10.2f}\n".format(
-                item["name"], item["quantity"], item["price"]))
-            subtotal += item["price"] * int(item["quantity"])
-        printer.text("-----------------------------------------------\n")
+            # print item details and calculate subtotal
+            subtotal = 0
+            # printer.text("-----------------------------------------------\n")
+            printer.text("{:<20}{:<8}{:<10}\n".format("Item", "Qty", "Price"))
+            printer.text("-----------------------------------------------\n")
+            for item in items:
+                printer.text("{:<20}{:<8}{:<10.2f}\n".format(
+                    item["name"], item["quantity"], item["price"]))
+                subtotal += item["price"] * int(item["quantity"])
+            printer.text("-----------------------------------------------\n")
 
-        # calculate tax and total
+            # calculate tax and total
 
-        total = subtotal
-        printer.text("{:<20}{:<10.2f}\n".format("Subtotal", subtotal))
-        printer.text("{:<20}{:<10.2f}\n".format("Total", total))
+            total = subtotal
+            printer.text("{:<20}{:<10.2f}\n".format("Subtotal", subtotal))
+            printer.text("{:<20}{:<10.2f}\n".format("Total", total))
 
-        # set up receipt footer
-        printer.text("-----------------------------------------------\n")
-        printer.text("Thank you for visiting!\n")
+            # set up receipt footer
+            printer.text("-----------------------------------------------\n")
+            printer.text("Thank you for visiting!\n")
 
-        # cut the paper
+            # cut the paper
 
-        printer.cut()  # close printer connection
-        printer.close()
-
-    except Exception as e:
-        print(f"Failed to print receipt: {e}")
-        if printer is not None:
+            printer.cut()  # close printer connection
             printer.close()
+
+        except Exception as e:
+            print(f"Failed to print receipt: {e}")
+            if printer is not None:
+                printer.close()
 
 
 client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
